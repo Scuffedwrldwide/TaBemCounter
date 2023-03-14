0fa795366db4e75a660a0e5ce66250b960eb365b
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2023-03-14 16:26:08 +0000

    started work on file saving

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define KEYWORD1 "Tá bem?"
 #define CHAR1 't'
@@ -17,12 +18,15 @@
 
 int input();
 void separator(char separator, int length);
+void fileWrite(FILE *table, int C1, int C2, int C3, int C4, int total);
 
 int main()
 {
+    FILE *table;
+    table = fopen("table.txt", "a");
     printf(TITLE);
     separator('=', WIDTH);
-    while (input() != 1)
+    while (input(&table) != 1)
     {
         /*recieve input*/
     }
@@ -36,7 +40,7 @@ void separator(char separator, int length)
     putchar('\n');
 }
 
-int input()
+int input(FILE *table)
 {
     int C1 = 0, C2 = 0, C3 = 0, C4 = 0, total = 0;
     char c;
@@ -82,6 +86,20 @@ int input()
 /*  printf("%s: %d\n", KEYWORD4, C4); */
     separator('=', WIDTH);
     printf("Total: %d\n", total);
+    fileWrite(table, C1, C2, C3, C4, total);
     return 1;
 }
 
+void fileWrite(FILE *table, int C1, int C2, int C3, int C4, int total)
+{
+
+    fprintf(table, "====================\n");
+    fprintf(table, "%-10s\t%4d\n", KEYWORD1, C1);
+    fprintf(table, "%-10s\t%4d\n", KEYWORD2, C2);
+    fprintf(table, "%-10s\t%4d\n", KEYWORD3, C3);
+    fprintf(table, "%-10s\t%4d\n", KEYWORD4, C4);
+    fprintf(table, "Total: %d\n", total);
+    fprintf(table, "====================\n");
+/*  printf("%s: %d\n", KEYWORD4, C4); */
+    
+}
